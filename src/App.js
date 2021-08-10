fd31901b5b645e40efe3512e925144648fc65198
S4ntiego <adamksiazek123@gmail.com>
2021-08-10 18:57:02 +0200

    added changes related to part 2 e)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
+import ErrorNotification from './components/ErrorNotification'
+import Notification from './components/Notification'
 import Persons from '../src/components/Persons'
 import PeopleAdd from '../src/components/PeopleAdd'
 import contactsService from '../src/services/contacts'
@@ -16,6 +18,8 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ personsFilter, setPersonsFilter ] = useState('')
+  const [ addedMessage, setAddedMessage ] = useState(null)
+  const [ errorMessage, setErrorMessage ] = useState(null)
 
   const personsNames = persons.map((person) => { return person.name })
 
@@ -57,6 +61,7 @@ const App = () => {
         .then(returnedPerson => {
           setPersons(persons.map(person => person.id !== personObject.id ? person : returnedPerson)
         )})
+        .catch(error => setErrorMessage(`Information of ${personObject.name} has already been removed from the server`))
       }
     } else {
     contactsService.create(personObject)
@@ -64,6 +69,7 @@ const App = () => {
       setPersons(persons.concat(returnedNote)))
       setNewName('')
       setNewNumber('')
+      setAddedMessage(`Added ${personObject.name}`)
     }
   }
 
@@ -74,11 +80,13 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <ErrorNotification message={errorMessage}/>
+      <Notification message={addedMessage}/>
           filter shown with <input value={personsFilter} onChange={handleFilterChange}/>
-      <h2>add a new</h2>
+      <h1>add a new</h1>
       <PeopleAdd newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} submitName={submitName}/>
-      <h2>Numbers</h2>
+      <h1>Numbers</h1>
       <ul>
       <Persons persons={personsToShow} handlePersonDelete={handlePersonDelete}/>
       </ul>
--- src/components/Person.js
+++ src/components/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Person = ({ name, number, handleDelete }) => {
   return (
     <div>
-    <li>{name} {number} <button onClick={handleDelete}>Delete</button></li>
+      <li className='person'>{name} {number} <button onClick={handleDelete}>Delete</button></li>
     </div>
   )
 }
